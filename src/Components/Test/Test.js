445c86766940668baeab30e4fde5f63f93e6ebc3
tonyg <aj.gianno@gmail.com>
2021-04-03 12:54:03 -0500

    test func

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import UserContext from '../../Context/UserContext';
+import TokenService from '../../Services/token-service';
+import { Link } from 'react-router-dom';
+import './Test.css';
 
 export default class Test extends Component {
   static contextType = UserContext;
 
+  renderTestFunc() {
+    return (
+      <div className='__test'>
+        <Link>User Data</Link>
+        <br />
+        <Link>User Data</Link>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <>
-        <h1>yesasdasdad</h1>
-      </>
+      <div>{TokenService.hasAuthToken() ? this.renderTestFunc() : null}</div>
     );
   }
 }
