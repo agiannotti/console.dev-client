7d3e1a0880046eeb1b2d995e61d905943e14fa46
tonyg <aj.gianno@gmail.com>
2021-04-05 14:04:11 -0500

    label

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,14 @@
-import "./Main.css";
-import React, { useState, useEffect } from "react";
-import Test from "../Test/Test";
-import codeSnips from "../../CodeSnips/CodeSnips";
+import './Main.css';
+import React, { useState, useEffect } from 'react';
+import Test from '../Test/Test';
+import codeSnips from '../../CodeSnips/CodeSnips';
 
 export default function Main() {
-  const [textInput, setTextInput] = useState("");
+  const [textInput, setTextInput] = useState('');
   const [codeSnip, setCodeSnip] = useState(
     codeSnips.snippets[Math.floor(Math.random() * codeSnips.snippets.length)]
   );
-  const [textColor, setTextColor] = useState("");
+  const [textColor, setTextColor] = useState('');
 
   const newSnip = codeSnips.snippets[0];
   const randomSnip =
@@ -22,9 +22,9 @@ export default function Main() {
         textInput.charAt(textInput.length - 1) !==
         codeSnip.charAt(textInput.length - 1)
       ) {
-        return setTextColor("red");
+        return setTextColor('red');
       } else {
-        return setTextColor("white");
+        return setTextColor('white');
       }
     }
   }, [codeSnip, textInput, newSnip]);
@@ -36,42 +36,44 @@ export default function Main() {
   const handleKeyDown = (e) => {
     if (e.keyCode === 9) {
       e.preventDefault();
-      setTextInput(textInput + "      ");
+      setTextInput(textInput + '      ');
     }
   };
 
   const handleRestartButton = (e) => {
-    setTextInput("");
+    setTextInput('');
   };
 
   const handleRandomizeButton = (e) => {
-    setTextInput("");
+    setTextInput('');
     setCodeSnip(randomSnip);
   };
 
   return (
     <div>
       <Test />
-      <div className="parent">
-        <pre style={{ color: textColor }} className="__code">
+      <div className='parent'>
+        <pre style={{ color: textColor }} className='__code'>
           {codeSnip}
         </pre>
       </div>
       <div>
         <form>
-          <textarea
-            type="text"
-            onChange={handleOnChange}
-            id="__text"
-            onKeyDown={handleKeyDown}
-            value={textInput}
-          />
+          <label for='typing_box'>
+            <textarea
+              type='text'
+              onChange={handleOnChange}
+              id='__text'
+              onKeyDown={handleKeyDown}
+              value={textInput}
+            />
+          </label>
         </form>
-        <div className="__buttons">
-          <button onClick={handleRestartButton} className="__restart">
+        <div className='__buttons'>
+          <button onClick={handleRestartButton} className='__restart'>
             Restart
           </button>
-          <button onClick={handleRandomizeButton} className="__randomize">
+          <button onClick={handleRandomizeButton} className='__randomize'>
             Randomize
           </button>
         </div>
