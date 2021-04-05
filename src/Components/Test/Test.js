f65c67a30183b99e99fb2c687c9a0ecb4362f115
tonyg <aj.gianno@gmail.com>
2021-04-05 12:55:12 -0500

    updated

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -1,23 +1,16 @@
-import React, { Component } from "react";
-import UserContext from "../../Context/UserContext";
-import TokenService from "../../Services/token-service";
+import React, { Component } from 'react';
+import UserContext from '../../Context/UserContext';
+import TokenService from '../../Services/token-service';
 // import { Link } from "react-router-dom";
-import "./Test.css";
+import './Test.css';
 
 export default class Test extends Component {
   static contextType = UserContext;
 
   renderTestFunc() {
     return (
-<<<<<<< HEAD
       <div className='__test'>
-        <Link>User Data</Link>
-=======
-      <div className="__test">
-        {/* <Link>User Data</Link> */}
         <br />
-        {/* <Link>User Data</Link> */}
->>>>>>> a8a52e82fb152bc0ea11bac0d3ac21d9bece3bb7
       </div>
     );
   }
